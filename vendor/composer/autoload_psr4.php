a1a3991d9a9d540824cc1d25744348cd1c4024f1
Krister Andersson <cyclonecode@gmail.com>
2021-05-26 18:22:35 +0200

    Update lock file

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Composer\\Installers\\' => array($vendorDir . '/composer/installers/src/Composer/Installers'),
     'CisionBlock\\' => array($baseDir . '/src'),
 );
