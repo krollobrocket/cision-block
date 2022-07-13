797378dd0898b0e04335f88f5d5af8a161d8413e
Krister Andersson <cyclonecode@gmail.com>
2022-07-13 14:47:01 +0200

    Update premium notice

--- readme.txt
+++ readme.txt
@@ -3,9 +3,9 @@ Contributors: cyclonecode
 Donate link: https://www.buymeacoffee.com/cyclonecode
 Tags: cision, feed, investor relation, press, stock, shortcode, widget, content
 Requires at least: 3.1.0
-Tested up to: 5.9
+Tested up to: 6.0.1
 Requires PHP: 5.6
-Stable tag: 2.7.0
+Stable tag: 2.7.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
--- src/Backend/Backend.php
+++ src/Backend/Backend.php
@@ -184,6 +184,7 @@ class Backend extends Singleton
                 <li><?php _e('Subscription module.', 'cision-block'); ?></li>
                 <li><?php _e('Shareholder module.', 'cision-block'); ?></li>
                 <li><?php _e('Ticker module.', 'cision-block'); ?></li>
+                <li><?php _e('Calendar module.', 'cision-block'); ?></li>
                 <li><?php _e('Free support, installation and configuration help.', 'cision-block'); ?></li>
             </ul>
         </div>
