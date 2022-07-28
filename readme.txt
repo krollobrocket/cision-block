c2ec6c3be17f9c87de97588f42787ea4d863d03e
Krister Andersson <cyclonecode@gmail.com>
2022-07-29 00:45:07 +0200

    Improve status page

--- readme.txt
+++ readme.txt
@@ -5,7 +5,7 @@ Tags: cision, feed, investor relation, press, stock, shortcode, widget, content
 Requires at least: 3.1.0
 Tested up to: 6.0.1
 Requires PHP: 5.6
-Stable tag: 2.7.1
+Stable tag: 2.7.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -394,5 +394,5 @@ This is something that is possible using the **Pro** version, since all news are
 
 == Changelog ==
 
-= 2.7.1
-- Add PHP 8.0 support.
+= 2.7.2
+- Improve status page.
--- src/Backend/templates/status.php
+++ src/Backend/templates/status.php
@@ -1,6 +1,63 @@
 <?php
 
-$settings = $this->settings->toJson();
+$settings = $this->settings->toOptionsArray();
+unset($settings['notes']);
+$output = '## Settings ##' . PHP_EOL;
+foreach ($settings as $key => $value) {
+    if (is_array($value)) {
+        $value = implode(', ', $value);
+    } else {
+        $value = is_bool($value) ? (string)$value : $value;
+    }
+    $output .= $key . ': ' . ((string)$value) . PHP_EOL;
+}
+$output .= PHP_EOL;
+
+$output .= '## Wordpress ##' . PHP_EOL;
+$output .= 'Version: ' . get_bloginfo('version') . PHP_EOL;
+$output .= 'Multisite: ' . (is_multisite() ? 'yes' : 'no') . PHP_EOL;
+$output .= 'Site address: ' . get_bloginfo('url') . PHP_EOL;
+$output .= 'Debug mode: ' . (WP_DEBUG ? 'yes' : 'no') . PHP_EOL;
+$output .= 'Memory limit: ' . WP_MEMORY_LIMIT . PHP_EOL;
+$output .= 'Cron: ' . (defined('DISABLE_WP_CRON') && DISABLE_WP_CRON ? 'no' : 'yes') . PHP_EOL;
+$output .= 'Language: ' . get_locale() . PHP_EOL;
+$output .= PHP_EOL;
+
+$output .= '## PHP ##' . PHP_EOL;
+$output .= 'System name: ' . php_uname() . PHP_EOL;
+$output .= 'Architecture: ' . (PHP_INT_SIZE === 8 ? 'x64' : 'x86') . PHP_EOL;
+$output .= 'Version: ' . phpversion() . PHP_EOL;
+$output .= 'Debug build: ' . __(defined('ZEND_DEBUG_BUILD') && ZEND_DEBUG_BUILD ? 'yes' : 'no') . PHP_EOL;
+$output .= 'Zend Engine version: ' . zend_version() . PHP_EOL;
+$output .= 'Server Api: ' . php_sapi_name() . PHP_EOL;
+$variables = [
+    'memory_limit',
+    'variables_order',
+    'max_execution_time',
+    'max_input_time',
+    'upload_max_filesize',
+    'post_max_size',
+    'safe_mode',
+    'enable_dl',
+    'register_globals',
+    'expose_php',
+    'display_errors',
+    'file_uploads',
+    'allow_url_fopen',
+    'allow_url_include',
+    'magic_quotes_gpc',
+];
+
+$extensions = get_loaded_extensions();
+natcasesort($extensions);
+
+$output .= PHP_EOL . '### Extensions ###' . PHP_EOL;
+$output .= implode("\n", $extensions) . PHP_EOL;
+
+$output .= PHP_EOL . '### Variables ###' . PHP_EOL;
+foreach ($variables as $variable) {
+    $output .= $variable . ': ' . ini_get($variable) . PHP_EOL;
+}
 
 ?>
 <div class="wrap">
@@ -11,7 +68,7 @@ $settings = $this->settings->toJson();
         <table class="form-table">
             <tr>
                 <td>
-                    <textarea name="settings" readonly><?php echo $settings; ?></textarea>
+                    <textarea name="settings" readonly><?php echo $output; ?></textarea>
                     <p class="description"><?php _e('If you need help, copy and paste the above information for faster support.', 'cision-block'); ?></p>
                 </td>
             </tr>
